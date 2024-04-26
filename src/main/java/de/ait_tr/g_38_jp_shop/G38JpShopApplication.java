3de8bd88a72174f297999fb98e9ef6e544c90ae6
Schneider Irene <schneider_irene@outlook.com>
2024-04-26 21:11:52 +0200

    Initial commit

---
+++ src/main/java/de/ait_tr/g_38_jp_shop/G38JpShopApplication.java
@@ -0,0 +1,13 @@
+package de.ait_tr.g_38_jp_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G38JpShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G38JpShopApplication.class, args);
+	}
+
+}
